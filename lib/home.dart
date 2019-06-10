7d27703068ab742265440f663781f43f2b3bec3d
Damodar Lohani <dlohani48@gmail.com>
2019-06-10 16:31:35 +0545

    simple compound animation   - scale and all

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart'; 
+import 'dart:math' as math;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() { 
+    super.initState();
+    _controller=AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200)
+    );
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -10,7 +27,35 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            
+            ScaleTransition(
+              scale: CurvedAnimation(
+                curve: Interval(0.0, 1.0, curve: Curves.easeOut),
+                parent: _controller
+              ),
+              child: Container(
+                width: 60,
+                height: 60,
+                color: Colors.red,
+              ),
+            ),
+            FloatingActionButton(
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (BuildContext context, Widget child) {
+                  return Transform(
+                    alignment: FractionalOffset.center,
+                    transform: Matrix4.rotationZ(_controller.value * .5 * math.pi),
+                    child: Icon(_controller.isDismissed ? Icons.play_arrow : Icons.close)
+                  );
+                },
+              ),
+              onPressed: (){
+                if(_controller.isDismissed)
+                  _controller.forward();
+                else
+                  _controller.reverse();
+              },
+            )
           ],
         ),
       ),
